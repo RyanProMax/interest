85ba5757d16a66b8b16e13c68734957106e541dc
张宇雁 <zhangyuyan.1024@bytedance.com>
2021-09-23 20:26:50 +0800

    stash

--- components/Puzzle/Avatar.js
+++ components/Puzzle/Avatar.js
@@ -4,74 +4,71 @@ import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import { generateBlobURL } from './utils';
 
 function getBase64(img, callback) {
-	const reader = new FileReader();
-	reader.addEventListener('load', () => callback(reader.result));
-	reader.readAsDataURL(img);
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
 }
 
 export default function Avatar({ onChange }) {
-	const [loading, setLoading] = useState(false);
-	const [url, setUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState('');
 
-	const uploadButton = (
-		<div>
-			{loading ? <LoadingOutlined /> : <PlusOutlined />}
-			<div style={{ marginTop: 8 }}>Upload</div>
-		</div>
-	);
+  const customRequest = e => {
+    e.onSuccess();
+  };
 
-	const handleChange = info => {
-		const isJpgOrPng =
-			info.file.type === 'image/jpeg' || info.file.type === 'image/png';
-		if (!isJpgOrPng) {
-			message.error('只支持图片格式');
-			info.fileList.pop();
-			return;
-		}
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
-		if (info.file.status === 'uploading') {
-			setLoading(true);
-			return;
-		}
-		if (info.file.status === 'done') {
-			/* getBase64(info.file.originFileObj, imageUrl => {
+  const handleChange = info => {
+    const isJpgOrPng = info.file.type === 'image/jpeg' || info.file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('只支持图片格式');
+      info.fileList.pop();
+      return;
+    }
+
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === 'done') {
+      /* getBase64(info.file.originFileObj, imageUrl => {
 				info.file.imageUrl = imageUrl;
 				setLoading(false);
 				setUrl(imageUrl);
 			}); */
-			const blobURL = generateBlobURL(info.file.originFileObj);
-			info.file.imageUrl = blobURL;
-			setLoading(false);
-			setUrl(blobURL);
-		}
-		onChange(info);
-	};
+      const blobURL = generateBlobURL(info.file.originFileObj);
+      info.file.imageUrl = blobURL;
+      setLoading(false);
+      setUrl(blobURL);
+    }
+    onChange(info);
+  };
 
-	return (
-		<Upload
-			name="avatar"
-			listType="picture-card"
-			className="interest-puzzle-picture"
-			showUploadList={false}
-			onChange={handleChange}
-		>
-			{url ? (
-				<img
-					src={url}
-					alt="avatar"
-					style={{ width: '100%' }}
-					onLoad={e => {
-						const { width, height, classList } = e.target;
-						if (width > height) {
-							classList.add('object-fit');
-						} else {
-							classList.remove('object-fit');
-						}
-					}}
-				/>
-			) : (
-				uploadButton
-			)}
-		</Upload>
-	);
+  return (
+    <Upload name='avatar' listType='picture-card' className='interest-puzzle-picture' showUploadList={false} onChange={handleChange} customRequest={customRequest}>
+      {url ? (
+        <img
+          src={url}
+          alt='avatar'
+          style={{ width: '100%' }}
+          onLoad={e => {
+            const { width, height, classList } = e.target;
+            if (width > height) {
+              classList.add('object-fit');
+            } else {
+              classList.remove('object-fit');
+            }
+          }}
+        />
+      ) : (
+        uploadButton
+      )}
+    </Upload>
+  );
 }
--- components/Puzzle/PictureWall.js
+++ components/Puzzle/PictureWall.js
@@ -4,74 +4,65 @@ import { PlusOutlined } from '@ant-design/icons';
 import { generateBlobURL } from './utils';
 
 function getBase64(file) {
-	return new Promise((resolve, reject) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = () => resolve(reader.result);
-		reader.onerror = error => reject(error);
-	});
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 }
 
 const uploadButton = (
-	<div>
-		<PlusOutlined />
-		<div style={{ marginTop: 8 }}>Upload</div>
-	</div>
+  <div>
+    <PlusOutlined />
+    <div style={{ marginTop: 8 }}>Upload</div>
+  </div>
 );
 
 export default function PictureWall({ onChange }) {
-	const [fileList, setFileList] = useState([]);
-	const [preview, setPreview] = useState('');
-	const [previewTitle, setPreviewTitle] = useState('');
+  const [fileList, setFileList] = useState([]);
+  const [preview, setPreview] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
 
-	const handlePreview = async file => {
-		if (!file.url && !file.preview) {
-			file.preview = await getBase64(file.originFileObj);
-		}
+  const customRequest = e => {
+    e.onSuccess();
+  };
 
-		setPreview(file.url || file.preview);
-		setPreviewTitle(
-			file.name || file.url.substring(file.url.lastIndexOf('/') + 1)
-		);
-	};
+  const handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
 
-	const handleCancel = () => setPreview('');
+    setPreview(file.url || file.preview);
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+  };
 
-	const handleChange = ({ file, fileList }) => {
-		const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-		if (!isJpgOrPng) {
-			message.error('只支持图片格式');
-			fileList.pop();
-			return;
-		}
+  const handleCancel = () => setPreview('');
 
-		fileList.forEach(file => {
-			file.blobURL = generateBlobURL(file.originFileObj);
-		});
-		onChange({ file, fileList });
-		setFileList(fileList);
-	};
+  const handleChange = ({ file, fileList }) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('只支持图片格式');
+      fileList.pop();
+      return;
+    }
 
-	return (
-		<>
-			<Upload
-				listType="picture-card"
-				fileList={fileList}
-				onPreview={handlePreview}
-				onChange={handleChange}
-				multiple
-			>
-				{/* {fileList.length >= 8 ? null : uploadButton} */}
-				{uploadButton}
-			</Upload>
-			<Modal
-				visible={!!preview}
-				title={previewTitle}
-				footer={null}
-				onCancel={handleCancel}
-			>
-				<img alt="example" style={{ width: '100%' }} src={preview} />
-			</Modal>
-		</>
-	);
+    fileList.forEach(file => {
+      file.blobURL = generateBlobURL(file.originFileObj);
+    });
+    onChange({ file, fileList });
+    setFileList(fileList);
+  };
+
+  return (
+    <>
+      <Upload customRequest={customRequest} listType='picture-card' fileList={fileList} onPreview={handlePreview} onChange={handleChange} multiple>
+        {/* {fileList.length >= 8 ? null : uploadButton} */}
+        {uploadButton}
+      </Upload>
+      <Modal visible={!!preview} title={previewTitle} footer={null} onCancel={handleCancel}>
+        <img alt='example' style={{ width: '100%' }} src={preview} />
+      </Modal>
+    </>
+  );
 }
