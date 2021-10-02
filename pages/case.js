7adc19f4109240a58b03d3269ad2ec5b9816e2f8
Ayan <627311610@qq.com>
2021-10-02 23:03:19 +0800

    feat: pixel video

--- pages/case.js
+++ pages/case.js
@@ -2,19 +2,25 @@ import Card from '../components/Card';
 import { basePath } from '../settings';
 
 export default function Case() {
-  const LIST = [
-    {
-      name: 'Puzzle',
-      imgSrc: `${basePath}/images/02.jpg`,
-      path: '/case/puzzle'
-    }
-  ];
+	const LIST = [
+		{
+			name: 'Puzzle',
+			imgSrc: `${basePath}/images/02.jpg`,
+			path: '/case/puzzle'
+		},
 
-  return (
-    <div className='interest-case'>
-      {LIST.map((props, idx) => (
-        <Card key={idx} {...props} />
-      ))}
-    </div>
-  );
+		{
+			name: 'Pixel Video',
+			imgSrc: `${basePath}/images/02.jpg`,
+			path: '/case/pixel-video'
+		}
+	];
+
+	return (
+		<div className="interest-case">
+			{LIST.map((props, idx) => (
+				<Card key={idx} {...props} style={{ margin: '0 10px 20px 10px' }} />
+			))}
+		</div>
+	);
 }
