58dbecf0612a4a7e03396903042f97381a20d6ad
Ryan <627311610@qq.com>
2021-10-06 00:45:40 +0800

    update: 优化 StringVideo

--- src/components/StringVideo/index.js
+++ src/components/StringVideo/index.js
@@ -14,6 +14,11 @@ let height = 0;
 export default function StringVideo(props) {
   const { isMobile } = props;
 
+  const isInited = useRef(false);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const timerRef = useRef(null);
+
   const [width, setWidth] = useState(500);
 
   useEffect(() => {
@@ -24,69 +29,51 @@ export default function StringVideo(props) {
     }
   }, [width, isMobile]);
 
-  const isInited = useRef(false);
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const timerRef = useRef(null);
-
-  useEffect(() => {
+  const handleRender = () => {
     const video = videoRef.current;
     const canvas = canvasRef.current;
 
-    const render = () => {
-      console.log('render');
-      // init canvas
-      if (!isInited.current) {
-        const { offsetHeight } = video;
-        height = offsetHeight;
-        canvas.setAttribute('height', height);
-        isInited.current = true;
+    console.log('render');
+    // init canvas
+    if (!isInited.current) {
+      const { offsetHeight } = video;
+      height = offsetHeight;
+      canvas.setAttribute('height', height);
+      isInited.current = true;
+    }
+
+    const ctx = canvas.getContext('2d');
+    if (!video || !canvas || !ctx) return;
+
+    // read video frame
+    ctx.drawImage(video, 0, 0, width, height);
+    const imgData = ctx.getImageData(0, 0, width, height).data;
+    ctx.clearRect(0, 0, width, height);
+    ctx.font = gap + 'px Verdana';
+    for (let h = 0; h < height; h += gap) {
+      for (let w = 0; w < width; w += gap) {
+        const position = (width * h + w) * 4;
+        const r = imgData[position],
+          g = imgData[position + 1],
+          b = imgData[position + 2];
+        // calculate gray
+        const gray = (r * 30 + g * 59 + b * 11 + 50) / 100;
+        const i = Math.min(
+          fillList.length - 1,
+          parseInt(gray / (255 / fillList.length))
+        );
+        // render
+        ctx.fillText(fillList[i], w, h);
       }
+    }
+    timerRef.current = requestAnimationFrame(handleRender);
+  };
 
-      const ctx = canvas.getContext('2d');
-      if (!video || !canvas || !ctx) return;
-
-      // read video frame
-      ctx.drawImage(video, 0, 0, width, height);
-      const imgData = ctx.getImageData(0, 0, width, height).data;
-      ctx.clearRect(0, 0, width, height);
-      ctx.font = gap + 'px Verdana';
-      for (let h = 0; h < height; h += gap) {
-        for (let w = 0; w < width; w += gap) {
-          const position = (width * h + w) * 4;
-          const r = imgData[position],
-            g = imgData[position + 1],
-            b = imgData[position + 2];
-          // calculate gray
-          const gray = (r * 30 + g * 59 + b * 11 + 50) / 100;
-          const i = Math.min(
-            fillList.length - 1,
-            parseInt(gray / (255 / fillList.length))
-          );
-          // render
-          ctx.fillText(fillList[i], w, h);
-        }
-      }
-      timerRef.current = requestAnimationFrame(render);
-    };
-
-    const stopRender = () => {
-      const timer = timerRef.current;
-      if (!timer) return;
-      cancelAnimationFrame(timer);
-    };
-
-    video.addEventListener('play', render);
-    video.addEventListener('pause', stopRender);
-    video.addEventListener('ended', stopRender);
-
-    return () => {
-      stopRender();
-      video.removeEventListener('play', render);
-      video.removeEventListener('pause', stopRender);
-      video.removeEventListener('ended', stopRender);
-    };
-  }, [width]);
+  const handleStopRender = () => {
+    const timer = timerRef.current;
+    if (!timer) return;
+    cancelAnimationFrame(timer);
+  };
 
   return (
     <Main
@@ -100,7 +87,10 @@ export default function StringVideo(props) {
           ref={videoRef}
           src="/interest/videos/01.mp4"
           style={{ width: width }}
-          controls="controls"
+          onPlay={handleRender}
+          onPause={handleStopRender}
+          onEnded={handleStopRender}
+          controls={true}
         ></video>
         <canvas
           ref={canvasRef}
