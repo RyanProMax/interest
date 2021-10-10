852a2f67862ad303a8f8cdbb311b8d12acc6aedd
Ayan <627311610@qq.com>
2021-10-10 18:49:53 +0800

    update: example cover

--- pages/example.js
+++ pages/example.js
@@ -6,28 +6,28 @@ export default function Example({ isMobile }) {
   const LIST = [
     {
       name: 'Puzzle',
-      imgSrc: `${basePath}/images/02.jpg`,
+      imgSrc: `${basePath}/images/puzzle.jpg`,
       path: '/example/puzzle'
     },
     {
       name: 'String Video',
-      imgSrc: `${basePath}/images/03.jpg`,
+      imgSrc: `${basePath}/images/string-video.jpg`,
       path: '/example/string-video'
     },
     {
       name: 'Generate GIF',
-      imgSrc: `${basePath}/images/03.jpg`,
+      imgSrc: `${basePath}/images/generate-gif.gif`,
       path: '/example/generate-gif'
     },
     {
       name: 'Mini Cooper',
-      imgSrc: `${basePath}/images/04.png`,
+      imgSrc: `${basePath}/images/mini-cooper.png`,
       path: '/example/mini-cooper'
     }
   ];
 
   return (
-    <div className='interest-example'>
+    <div className="interest-example">
       <Row gutter={[16, 16]} style={{ maxWidth: 1280, width: '100%' }}>
         {LIST.map((p, i) => (
           <Col key={i} xs={24} md={12} lg={8} xl={6}>
