055874c3ea95ade43ab8c8cfd7f59ee8bf519cfc
张宇雁 <zhangyuyan.1024@bytedance.com>
2021-10-19 16:30:21 +0800

    update: bingding CNAME

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development';
-const basePath = '/interest';
+const basePath = '';
 
 const RESOURCE_DOMAIN = 'https://cdn.zhangyuyan.cn';
 const TEST_DOMAIN = `${basePath}/test`;
